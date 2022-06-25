7f5745262f7239fbe339e996f3e8fa9cc3cfc867
Prophet <finn@famschneider.info>
2022-06-25 20:47:26 +0200

    Add nix derivations

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ lib, nix-gitignore, buildDunePackage }:
+
+buildDunePackage rec {
+  pname = "ocaml_bestline";
+  version = "0.1.0";
+  duneVersion = "3";
+
+  minimalOCamlVersion = "4.14";
+
+  src = nix-gitignore.gitignoreFilterSource lib.cleanSourceFilter [ ] ./.;
+  checkInputs = [ ];
+  propagatedBuildInputs = [ ];
+  passthru.checkInputs = checkInputs;
+
+  meta = { homepage = "https://github.com/Innf107/ocaml-bestline"; };
+}
